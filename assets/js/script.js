3f0a773d23a524d569f6e7c073650936d0b502d6
CamDiggidyDawg <camrcrichton@gmail.com>
2023-01-23 15:41:00 +0000

    Add inital function names to the script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,29 @@
+// Check if the DOM content is loaded
 
 
+// function name and idea from Love Maths project
+function runGame()
+
+
+// function name and idea from Love Maths project
+function checkAnswer()
+
+
+// function name and idea from Love Maths project
+function calculateAnswer()
+
+
+// function name and idea from Love Maths project
+function playerScore()
+
+
+// function name and idea from Love Maths project
+function computerScore()
+
+
+// function name and idea from Love Maths project
+function targetNumber()
+
+
+// function name and idea from Love Maths project
+function gameOver()
