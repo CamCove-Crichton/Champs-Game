d91b27cac6466a5296e3795832f3efc654b50670
CamDiggidyDawg <camrcrichton@gmail.com>
2023-01-24 07:25:16 +0000

    Update the DOMContentLoaded event listener to work when the submit button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,42 @@
 // Check if the DOM content is loaded
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
 
-    for(button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-entry") === "submit") {
-                alert("Content Loaded");
-            }
-        })
-    }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.getElementById("submit");
+
+    button.addEventListener("click", function() {
+        if (this.getAttribute("data-entry") === "submit") {
+            alert("DOM content loaded");
+            console.log("DOM Content loaded");
+        }
+    })
 })
 
-// function name and idea from Love Maths project
-function runGame()
-
 
 // function name and idea from Love Maths project
-function checkAnswer()
+// function runGame()
 
 
 // function name and idea from Love Maths project
-function calculateAnswer()
+// function checkAnswer()
 
 
 // function name and idea from Love Maths project
-function playerScore()
+// function calculateAnswer()
 
 
 // function name and idea from Love Maths project
-function computerScore()
+// function playerScore()
 
 
 // function name and idea from Love Maths project
-function targetNumber()
+// function computerScore()
 
 
 // function name and idea from Love Maths project
-function gameOver()
+// function targetNumber()
+
+
+// function name and idea from Love Maths project
+// function gameOver()
